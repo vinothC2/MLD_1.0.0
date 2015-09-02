7607204714b77c50886a41bfb15bc16eb39ec2c7
vinothC2 <chandranvinoth08@gmail.com>
2015-09-02 16:01:42 +0530

    test

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,20 @@
  * Controller of the mldApp
  */
 angular.module('mldApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http, $location) {
+    
+	$scope.login = function () {
+		var url = 'http://localhost:1212/api/login';
+		var data = {userName:$scope.userName,password:$scope.password};
+		$http.post(url,data)
+		.success(function(data) {
+			console.log(data.isLogged);
+        }).error(function(res) {
+			console.log('Error');
+        });
+	}
+	
+	$("#loginButton").click(function(){
+		$scope.login();
+	});
   });
